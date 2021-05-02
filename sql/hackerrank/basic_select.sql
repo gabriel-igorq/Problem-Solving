2e49fc780c673d10771fc708a730089e13ca81ba
Gabriel Igor <gabriel.igorq@gmail.com>
2021-05-02 16:07:27 -0300

    Adding more problems to sql

--- sql/hackerrank/basic_select.sql
+++ sql/hackerrank/basic_select.sql
@@ -57,3 +57,71 @@ ORDER BY LENGTH(city), city LIMIT 1;
 SELECT city, LENGTH(city)
 FROM station
 ORDER BY LENGTH(city) DESC, city LIMIT 1;
+
+-- Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. 
+-- Your result cannot contain duplicates.
+SELECT DISTINCT city
+FROM station
+WHERE city REGEXP '^[aeiou]'
+
+-- Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. 
+-- Your result cannot contain duplicates
+SELECT DISTINCT city
+FROM station
+WHERE city REGEXP '[aeiou]$'
+
+-- Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) 
+-- as both their first and last characters. Your result cannot contain duplicates.
+SELECT DISTINCT city
+FROM station
+WHERE city REGEXP '^[aeiou].*[aeiou]$'
+
+-- Query the list of CITY names from STATION that do not start with vowels. 
+-- Your result cannot contain duplicates.
+SELECT DISTINCT city
+FROM station
+WHERE city NOT REGEXP '^[aeiou]'
+
+-- Query the list of CITY names from STATION that do not end with vowels. 
+-- Your result cannot contain duplicates.
+SELECT DISTINCT city
+FROM station
+WHERE city NOT REGEXP '[aeiou]$'
+
+-- Query the list of CITY names from STATION that either do not start with vowels or do not end with 
+-- vowels. Your result cannot contain duplicates.
+SELECT DISTINCT city
+FROM station
+WHERE city NOT REGEXP '^[aeiou].*[aeiou]$'
+
+-- Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. -- Your result cannot contain duplicates
+SELECT DISTINCT city
+FROM station
+WHERE city REGEXP '^[^aeiou].*[^aeiou]$'
+
+-- Query the Name of any student in STUDENTS who scored higher than 75 Marks. Order your output by the 
+-- last three characters of each name. If two or more students both have names ending in the same last 
+-- three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+SELECT name
+FROM students
+WHERE marks > 75
+ORDER BY RIGHT(name, 3), id
+
+-- Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in 
+-- alphabetical order.
+SELECT name
+FROM employee
+ORDER BY name
+
+-- Write a query that prints a list of employee names (i.e.: the name attribute) for employees in Employee having 
+-- a salary greater than $2000 per month who have been employees for less than 10 months. Sort your result by 
+-- ascending employee_id.
+SELECT name
+FROM employee
+WHERE salary > 2000 and months < 10
+ORDER BY employee_id
+
+-- Write a query identifying the type of each record in the TRIANGLES table using its three side lengths.
+SELECT
+IF (a+b > c and b+c > a and c+a > b, IF(a=b and b=c, 'Equilateral', IF(a=b or b=c or a=c, 'Isosceles', 'Scalene')), 'Not A Triangle')
+FROM triangles
